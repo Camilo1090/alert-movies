3991b405be570e818af0a009acf1e9ae47d38a6c
Camilo1090 <camilo.ruiz.casanova@correounivalle.edu.co>
2017-11-02 11:08:54 -0500

    fixed header position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Alert Movies';

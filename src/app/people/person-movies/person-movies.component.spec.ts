cac49cc49cbd6130824ecbba36888dc63fad9a75
Camilo1090 <camilo.ruiz.casanova@correounivalle.edu.co>
2017-12-18 15:44:50 -0500

    added unit tests for person movies comp. (wip)

---
+++ src/app/people/person-movies/person-movies.component.spec.ts
@@ -0,0 +1,405 @@
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+
+import { APP_BASE_HREF, CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../../app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import {
+  MatButtonModule, MatCardModule, MatDialogModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule,
+  MatMenuModule, MatSelectModule, MatSidenavModule, MatTabsModule, MatToolbarModule, MatTooltipModule
+} from '@angular/material';
+import {
+  CovalentChipsModule, CovalentLayoutModule, CovalentLoadingModule, CovalentMediaModule, CovalentMenuModule,
+  CovalentNotificationsModule, CovalentPagingModule, CovalentSearchModule, IPageChangeEvent, TdMediaService,
+} from '@covalent/core';
+import { CovalentHttpModule } from '@covalent/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModalGalleryModule } from 'angular-modal-gallery';
+
+import * as personDetails from '../../testing/person-details.json';
+
+import { TrendingComponent } from '../../trending/trending.component';
+import { ListMoviesComponent } from '../../movies/list-movies/list-movies.component';
+import { MovieDetailsComponent } from '../../movies/movie-details/movie-details.component';
+import { ListPeopleComponent } from '../list-people/list-people.component';
+import { ListSeriesComponent } from '../../series/list-series/list-series.component';
+import { FooterComponent } from '../../footer/footer.component';
+import { MovieImagesComponent } from '../../movies/movie-images/movie-images.component';
+import { MovieVideosComponent } from '../../movies/movie-videos/movie-videos.component';
+import { MovieCastComponent } from '../../movies/movie-cast/movie-cast.component';
+import { MovieReviewsComponent } from '../../movies/movie-reviews/movie-reviews.component';
+import { MovieRecommendationsComponent } from '../../movies/movie-recommendations/movie-recommendations.component';
+import { SeriesDetailsComponent } from '../../series/series-details/series-details.component';
+import { SeriesImagesComponent } from '../../series/series-images/series-images.component';
+import { SeriesVideosComponent } from '../../series/series-videos/series-videos.component';
+import { FormatStringPipe } from '../../shared/format-string/format-string.pipe';
+import { SeriesCastComponent } from '../../series/series-cast/series-cast.component';
+import { SeriesRecommendationsComponent } from '../../series/series-recommendations/series-recommendations.component';
+import { PersonDetailsComponent } from '../person-details/person-details.component';
+import { PersonMoviesComponent } from './person-movies.component';
+import { PersonSeriesComponent } from '../person-series/person-series.component';
+import { PersonImagesComponent } from '../person-images/person-images.component';
+import { SearchComponent } from '../../search/search.component';
+import { SearchBarComponent } from '../../search/search-bar/search-bar.component';
+import { NotFoundComponent } from '../../shared/not-found/not-found.component';
+import { DiscoverComponent } from '../../discover/discover.component';
+import { CustomCardComponent } from '../../shared/custom-card/custom-card.component';
+import { LimitTextComponent } from '../../shared/limit-text/limit-text.component';
+import { API } from '../../shared/api/api';
+import { PeopleService } from '../shared/people.service';
+
+
+fdescribe('PersonMovies component test', () => {
+  let component: PersonMoviesComponent;
+  let fixture: ComponentFixture<PersonMoviesComponent>;
+
+  // Spy creation
+
+  // service
+  const getPersonMoviesSpy = jasmine.createSpy('getPersonMovies')
+    .and.returnValue(Observable.of(personDetails));
+
+  // TdMediaQuery
+  const mediaQuerySpy = jasmine.createSpy('query')
+    .and.returnValue(false);
+  const mediaRegisterQuerySpy = jasmine.createSpy('registerQuery')
+    .and.returnValue(Observable.of(false));
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        ListMoviesComponent,
+        FooterComponent,
+        TrendingComponent,
+        ListSeriesComponent,
+        ListPeopleComponent,
+        MovieDetailsComponent,
+        ListPeopleComponent,
+        MovieImagesComponent,
+        MovieVideosComponent,
+        MovieCastComponent,
+        MovieReviewsComponent,
+        MovieRecommendationsComponent,
+        SeriesDetailsComponent,
+        SeriesImagesComponent,
+        SeriesVideosComponent,
+        FormatStringPipe,
+        SeriesCastComponent,
+        SeriesRecommendationsComponent,
+        PersonDetailsComponent,
+        PersonMoviesComponent,
+        PersonSeriesComponent,
+        PersonImagesComponent,
+        SearchComponent,
+        SearchBarComponent,
+        NotFoundComponent,
+        DiscoverComponent,
+        CustomCardComponent,
+        LimitTextComponent
+      ],
+      imports: [
+        BrowserModule,
+        CommonModule,
+        AppRoutingModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        NgbModule.forRoot(),
+        ModalGalleryModule.forRoot(),
+        MatButtonModule,
+        MatSidenavModule,
+        MatTooltipModule,
+        MatDialogModule,
+        MatListModule,
+        MatCardModule,
+        MatIconModule,
+        MatInputModule,
+        MatMenuModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatGridListModule,
+        MatSelectModule,
+        CovalentLayoutModule,
+        CovalentMenuModule,
+        CovalentSearchModule,
+        CovalentHttpModule,
+        CovalentMediaModule,
+        CovalentPagingModule,
+        CovalentLoadingModule,
+        CovalentNotificationsModule,
+        CovalentChipsModule
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        {
+          provide: PeopleService, useClass: class {
+            getPersonMovies = getPersonMoviesSpy;
+          }
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: Observable.of({})
+          }
+        },
+        {
+          provide: TdMediaService, useClass: class {
+            query = mediaQuerySpy;
+            registerQuery = mediaRegisterQuerySpy;
+          }
+        },
+      ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PersonMoviesComponent);
+    component = fixture.componentInstance;
+    // fixture.detectChanges();
+  });
+  describe('WHEN the component is created', () => {
+    it('SHOULD create the global variables', () => {
+      expect(component)
+        .toBeTruthy();
+      expect(component.columns)
+        .toBeUndefined();
+      expect(component._querySubscription)
+        .toBeUndefined();
+
+      expect(component.movies)
+        .toEqual([]);
+      expect(component.apiImg)
+        .toEqual(API.apiImg + 'w500');
+    });
+  });
+
+  describe('WHEN ngOnInit function is called', () => {
+    beforeEach(() => {
+      spyOn(component, 'registerLoading').calls.reset();
+      spyOn(component, 'updatePersonMovies').calls.reset();
+      spyOn(component, 'checkScreen').calls.reset();
+      spyOn(component, 'watchScreen').calls.reset();
+    });
+    it('SHOULD call functions', fakeAsync(() => {
+      component.ngOnInit();
+      tick();
+
+      expect(component.registerLoading)
+        .toHaveBeenCalledTimes(1);
+      expect(component.updatePersonMovies)
+        .toHaveBeenCalledTimes(1);
+      expect(component.updatePersonMovies)
+        .toHaveBeenCalledWith(1);
+      expect(component.checkScreen)
+        .toHaveBeenCalledTimes(1);
+      expect(component.watchScreen)
+        .toHaveBeenCalledTimes(1);
+    }));
+  });
+
+  describe('WHEN updatePersonMovies function is called', () => {
+    let id: number;
+    beforeEach(() => {
+      getPersonMoviesSpy.calls.reset();
+      spyOn(component, 'resolveLoading').calls.reset();
+      component.movies = [];
+    });
+    it('SHOULD call service', () => {
+      id = 10;
+      component.route.params = Observable.of({ id: id });
+      component.updatePersonMovies();
+
+      expect(component.peopleService.getPersonMovies)
+        .toHaveBeenCalledTimes(1);
+      expect(component.peopleService.getPersonMovies)
+        .toHaveBeenCalledWith(id);
+      expect(component.resolveLoading)
+        .toHaveBeenCalledTimes(1);
+    });
+    it('SHOULD set values', () => {
+      component.updatePersonMovies();
+
+      expect(component.movies)
+        .toEqual((<any>personDetails).cast);
+    });
+    it('SHOULD call sort function', () => {
+      const a = {popularity: 1};
+      const b = {popularity: 2};
+      spyOn((<any>personDetails).movies.cast, 'sort').and.callFake(fn => fn(a, b));
+      component.updatePersonMovies();
+
+      expect((<any>personDetails).movies.cast.sort)
+        .toHaveBeenCalledTimes(1);
+    });
+    it('SHOULD handle error', () => {
+      getPersonMoviesSpy.and.returnValue(Observable.throw('test error'));
+      component.updatePersonMovies();
+
+      expect(component.resolveLoading)
+        .toHaveBeenCalledTimes(1);
+    });
+  });
+
+  // describe('WHEN changePage function is called', () => {
+  //   let event: IPageChangeEvent;
+  //   beforeEach(() => {
+  //     event = {
+  //       page: 1,
+  //       maxPage: 1,
+  //       pageSize: 20,
+  //       total: 1,
+  //       fromRow: 1,
+  //       toRow: 1
+  //     };
+  //     spyOn(component, 'registerLoading').calls.reset();
+  //     spyOn(component, 'updateMovieRecommendations').calls.reset();
+  //   });
+  //   it('SHOULD set internal values and call internal functions', () => {
+  //     component.changePage(event);
+  //
+  //     expect(component.currentPage)
+  //       .toEqual(event.page);
+  //     expect(component.registerLoading)
+  //       .toHaveBeenCalledTimes(1);
+  //     expect(component.updateMovieRecommendations)
+  //       .toHaveBeenCalledTimes(1);
+  //     expect(component.updateMovieRecommendations)
+  //       .toHaveBeenCalledWith(event.page);
+  //   });
+  // });
+  //
+  // describe('WHEN checkScreen function is called', () => {
+  //   beforeEach(() => {
+  //     spyOn(component._ngZone, 'run').and.callFake(fn => fn());
+  //     mediaQuerySpy.calls.reset();
+  //   });
+  //   it('SHOULD call internal functions', () => {
+  //     component.checkScreen();
+  //
+  //     expect(component._ngZone.run)
+  //       .toHaveBeenCalledTimes(1);
+  //     expect(component._mediaService.query)
+  //       .toHaveBeenCalledTimes(4);
+  //   });
+  //   it('SHOULD set internal values when query is (max-width: 600px)', () => {
+  //     mediaQuerySpy.and.callFake((query: string) => query === '(max-width: 600px)');
+  //     component.checkScreen();
+  //
+  //     expect(component.columns)
+  //       .toEqual(1);
+  //   });
+  //   it('SHOULD set internal values when query is gt-xs', () => {
+  //     mediaQuerySpy.and.callFake((query: string) => query === 'gt-xs');
+  //     component.checkScreen();
+  //
+  //     expect(component.columns)
+  //       .toEqual(2);
+  //   });
+  //   it('SHOULD set internal values when query is gt-sm', () => {
+  //     mediaQuerySpy.and.callFake((query: string) => query === 'gt-sm');
+  //     component.checkScreen();
+  //
+  //     expect(component.columns)
+  //       .toEqual(3);
+  //   });
+  //   it('SHOULD set internal values when query is gt-md', () => {
+  //     mediaQuerySpy.and.callFake((query: string) => query === 'gt-md');
+  //     component.checkScreen();
+  //
+  //     expect(component.columns)
+  //       .toEqual(4);
+  //   });
+  // });
+  //
+  // describe('WHEN watchScreen function is called', () => {
+  //   beforeEach(() => {
+  //     spyOn(component._ngZone, 'run').and.callFake(fn => fn());
+  //     mediaRegisterQuerySpy.calls.reset();
+  //   });
+  //   it('SHOULD call internal functions', () => {
+  //     component.watchScreen();
+  //
+  //     expect(component._mediaService.registerQuery)
+  //       .toHaveBeenCalledTimes(4);
+  //     expect(component._ngZone.run)
+  //       .toHaveBeenCalledTimes(4);
+  //   });
+  //   it('SHOULD set internal values when query is (max-width: 600px)', () => {
+  //     mediaRegisterQuerySpy.and.callFake((query: string) => {
+  //       if (query === '(max-width: 600px)') {
+  //         return Observable.of(true);
+  //       }
+  //       return Observable.of(false);
+  //     });
+  //     component.watchScreen();
+  //
+  //     expect(component.columns)
+  //       .toEqual(1);
+  //   });
+  //   it('SHOULD set internal values when query is gt-xs', () => {
+  //     mediaRegisterQuerySpy.and.callFake((query: string) => {
+  //       if (query === 'gt-xs') {
+  //         return Observable.of(true);
+  //       }
+  //       return Observable.of(false);
+  //     });
+  //     component.watchScreen();
+  //
+  //     expect(component.columns)
+  //       .toEqual(2);
+  //   });
+  //   it('SHOULD set internal values when query is gt-sm', () => {
+  //     mediaRegisterQuerySpy.and.callFake((query: string) => {
+  //       if (query === 'gt-sm') {
+  //         return Observable.of(true);
+  //       }
+  //       return Observable.of(false);
+  //     });
+  //     component.watchScreen();
+  //
+  //     expect(component.columns)
+  //       .toEqual(3);
+  //   });
+  //   it('SHOULD set internal values when query is gt-md', () => {
+  //     mediaRegisterQuerySpy.and.callFake((query: string) => {
+  //       if (query === 'gt-md') {
+  //         return Observable.of(true);
+  //       }
+  //       return Observable.of(false);
+  //     });
+  //     component.watchScreen();
+  //
+  //     expect(component.columns)
+  //       .toEqual(4);
+  //   });
+  // });
+  //
+  // describe('WHEN registerLoading function is called', () => {
+  //   beforeEach(() => {
+  //     spyOn(component._loadingService, 'register');
+  //     component.registerLoading();
+  //   });
+  //   it('SHOULD call functions', () => {
+  //     expect(component._loadingService.register)
+  //       .toHaveBeenCalledTimes(1);
+  //     expect(component._loadingService.register)
+  //       .toHaveBeenCalledWith('movie-recommendations');
+  //   });
+  // });
+  //
+  // describe('WHEN resolveMoviesLoading function is called', () => {
+  //   beforeEach(() => {
+  //     spyOn(component._loadingService, 'resolve');
+  //     component.resolveLoading();
+  //   });
+  //   it('SHOULD call functions', () => {
+  //     expect(component._loadingService.resolve)
+  //       .toHaveBeenCalledTimes(1);
+  //     expect(component._loadingService.resolve)
+  //       .toHaveBeenCalledWith('movie-recommendations');
+  //   });
+  // });
+});
